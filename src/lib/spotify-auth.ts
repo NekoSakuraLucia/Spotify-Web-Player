052040f4d6387673ae1dee4a5897eb51503a8b88
NekoSakuraLucia <mokurolucia@gmail.com>
2025-06-05 21:52:06 +0700

    เพิ่มประเภท constant นอกฟังก์ชันแทน

--- src/lib/spotify-auth.ts
+++ src/lib/spotify-auth.ts
@@ -2,12 +2,17 @@
 import { redirect } from 'next/navigation';
 import qs from 'querystring';
 
+// Client ID
+const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID as string;
+// Redirect URI
+const REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI as string;
+
 export default async function SpotifyAuth(): Promise<never> {
     const queryParam = {
-        client_id: process.env.SPOTIFY_CLIENT_ID as string,
+        client_id: CLIENT_ID,
         scope: 'user-read-private user-read-email',
         response_type: 'code',
-        redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+        redirect_uri: REDIRECT_URI,
     };
     return redirect(
         'https://accounts.spotify.com/authorize?' + qs.stringify(queryParam)
