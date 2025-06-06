c7c37bedc504f18c73864a620c27ba53a0351e53
NekoSakuraLucia <mokurolucia@gmail.com>
2025-06-06 22:39:43 +0700

    ปรับแก้จาก uri เต็มมาใช้ base_uri ที่กำหนด

--- src/lib/spotify-auth.ts
+++ src/lib/spotify-auth.ts
@@ -2,6 +2,9 @@
 import { redirect } from 'next/navigation';
 import qs from 'querystring';
 
+// BASE_URI
+import { base_uri } from '@/lib/base.api';
+
 // Client ID
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID as string;
 // Redirect URI
@@ -10,11 +13,11 @@ const REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI as string;
 export default async function SpotifyAuth(): Promise<never> {
     const queryParam = {
         client_id: CLIENT_ID,
-        scope: 'user-read-private user-read-email',
+        scope: 'user-read-private user-read-email user-top-read',
         response_type: 'code',
         redirect_uri: REDIRECT_URI,
     };
     return redirect(
-        'https://accounts.spotify.com/authorize?' + qs.stringify(queryParam)
+        `${base_uri.spotify.accounts_uri}/authorize?` + qs.stringify(queryParam)
     );
 }
