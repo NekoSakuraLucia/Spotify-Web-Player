ce250fd78554bed3c6a54e76a9ba74e3f1b75238
NekoSakuraLucia <mokurolucia@gmail.com>
2025-06-08 19:29:29 +0700

    เพิ่ม theme dark และ tailwind animate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default async function RootLayout({
     )?.value;
 
     return (
-        <html lang='en'>
+        <html lang='en' className='dark'>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
