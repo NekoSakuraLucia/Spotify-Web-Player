5218bdb84f4343bf0dfee3481951081257f308bc
NekoSakuraLucia <mokurolucia@gmail.com>
2025-06-08 23:12:17 +0700

    ปรับปรุงการใส่ scope เล็กน้อย

--- src/lib/spotify-auth.ts
+++ src/lib/spotify-auth.ts
@@ -10,10 +10,17 @@ const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID as string;
 // Redirect URI
 const REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI as string;
 
+const AuthScope = [
+    'user-read-private',
+    'user-read-email',
+    'user-top-read',
+    'user-read-currently-playing',
+] as const;
+
 export default async function SpotifyAuth(): Promise<never> {
     const queryParam = {
         client_id: CLIENT_ID,
-        scope: 'user-read-private user-read-email user-top-read',
+        scope: AuthScope.join(' '),
         response_type: 'code',
         redirect_uri: REDIRECT_URI,
     };
