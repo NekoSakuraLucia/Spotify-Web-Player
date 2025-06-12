44efc295ddb67bb3e436e7fb01d4d7fdd428865b
NekoSakuraLucia <mokurolucia@gmail.com>
2025-06-12 18:45:51 +0700

    เพิ่มไฟล์ constant สำหรับเก็บค่าคงที่ และ auth

---
+++ src/lib/constant.ts
@@ -0,0 +1,9 @@
+const AuthScope = [
+    'user-read-private',
+    'user-read-email',
+    'user-top-read',
+    'user-read-currently-playing',
+    'user-read-playback-state',
+] as const;
+
+export { AuthScope }
--- src/lib/spotify-auth.ts
+++ src/lib/spotify-auth.ts
@@ -5,18 +5,13 @@ import qs from 'querystring';
 // BASE_URI
 import { base_uri } from '@/lib/base.api';
 
+// constant
+import { AuthScope } from '@/lib/constant';
+
 // Client ID
 const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID as string;
 // Redirect URI
 const REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI as string;
-
-const AuthScope = [
-    'user-read-private',
-    'user-read-email',
-    'user-top-read',
-    'user-read-currently-playing',
-    'user-read-playback-state',
-] as const;
 
 export default async function SpotifyAuth(): Promise<never> {
     const queryParam = {
@@ -25,8 +20,7 @@ export default async function SpotifyAuth(): Promise<never> {
         response_type: 'code',
         redirect_uri: REDIRECT_URI,
     };
-    return redirect(
-        `${base_uri.spotify.accounts_uri}/authorize?` +
-            qs.stringify(queryParam)
-    );
+
+    const authorization = `${base_uri.spotify.accounts_uri}/authorize?` + qs.stringify(queryParam)
+    return redirect(authorization);
 }
