b68a688c51bd1adeeca95893e765eac67d646507
NekoSakuraLucia <mokurolucia@gmail.com>
2025-06-15 21:36:55 +0700

    ย้าย export มาใส่ในฟังก์ชันเลย

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,9 +1,7 @@
-const AuthScope = [
+export const AuthScope = [
     'user-read-private',
     'user-read-email',
     'user-top-read',
     'user-read-currently-playing',
     'user-read-playback-state',
 ] as const;
-
-export { AuthScope };
